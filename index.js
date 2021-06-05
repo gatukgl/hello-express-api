14a3aec75358e80d5f2296c6c455f4e963fe6927
Gatuk Chattanon <gatukgl@gmail.com>
2021-06-05 11:03:32 +0700

    Support edit book

--- index.js
+++ index.js
@@ -13,11 +13,26 @@ app.get("/", (req, res) => {
   res.json(returnValue);
 });
 
+app.get("books/:id", (req, res) => {
+  const { id } = req.params;
+  const statement = db.prepare(`SELECT * FROM books WHERE id = ${id}`);
+  const book = statement.get();
+  res.json(book);
+});
+
 app.get("/books", (req, res) => {
-    const statement = db.prepare("SELECT * from books")
-    const books = statement.all()
-    res.json(books)
-})
+  const statement = db.prepare("SELECT * from books");
+  const books = statement.all();
+  res.json(books);
+});
+
+app.patch("/books/:id", (req, res) => {
+  const { id } = req.params;
+  const { description } = req.body;
+  const statement = db.prepare("UPDATE books SET description = ? WHERE id = ?");
+  const info = statement.run(description, id);
+  res.json(info);
+});
 
 app.post("/new", (req, res) => {
   const { title, description } = req.body;
