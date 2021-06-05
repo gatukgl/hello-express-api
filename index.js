d7e6777b4d04b7d001e48ad2c4e4dffae57ed085
Gatuk Chattanon <gatukgl@gmail.com>
2021-06-05 10:49:47 +0700

    Support save book to database

--- index.js
+++ index.js
@@ -1,14 +1,25 @@
 const express = require("express");
+const db = require("./db");
 
 const app = express();
 const port = 3000;
 
+app.use(express.json());
 app.get("/", (req, res) => {
   const returnValue = {
     title: "Javascrip Bonus",
     description: "Lorem ipsum",
   };
   res.json(returnValue);
+});
+
+app.post("/new", (req, res) => {
+  const { title, description } = req.body;
+  const statement = db.prepare(
+    "INSERT INTO books (title, description) VALUES (?, ?)"
+  );
+  const info = statement.run(title, description);
+  res.json(info);
 });
 
 app.listen(port, () => {
